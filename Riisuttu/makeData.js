60665b93cdad3e131b77e5f9f71291f522c42698
kammotus <kihyvari@gmail.com>
2020-04-08 23:06:26 +0300

    Siivousta

--- Riisuttu/makeData.js
+++
@@ -1,5 +0,0 @@
-export default function makeData() {
-	fetch("https://rata.digitraffic.fi/api/v1/live-trains?station=JNS")
-		.then(response => response.json())
-		.then(data => console.log(data));
-}
